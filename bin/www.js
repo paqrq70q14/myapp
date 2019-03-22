9ee4f6dfcd2e065564b0cb2daa2f506976f09abc
patxing <476522933@qq.com>
2019-03-22 23:25:38 +0800

    更新

--- bin/www.js
+++ bin/www.js
@@ -4,14 +4,14 @@
 var app = require('../app.js');
 const http = require('http');
 
-var port = "3300";
+var port = "3500";
 
 // Create Server
 var server = http.createServer(app);
 
 // Listen 
 server.listen(port, function(err){
-    console.log("http://localhost:3300");
+    console.log("http://localhost:3500");
    
 });
 
