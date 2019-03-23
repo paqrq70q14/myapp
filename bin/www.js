a40fb7a94f913ad7f231c0cedf8e212d53d0a8be
patxing <476522933@qq.com>
2019-03-23 16:41:22 +0800

    测试更新

--- bin/www.js
+++ bin/www.js
@@ -4,14 +4,14 @@
 var app = require('../app.js');
 const http = require('http');
 
-var port = "3500";
+var port = "3307";
 
 // Create Server
 var server = http.createServer(app);
 
 // Listen 
 server.listen(port, function(err){
-    console.log("http://localhost:3500");
+    console.log("http://localhost:3307");
    
 });
 
